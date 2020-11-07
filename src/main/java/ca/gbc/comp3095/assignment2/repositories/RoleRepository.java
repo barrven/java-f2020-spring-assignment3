6a4228a072a552ee992074b8a0cde53312b97a5c
KevinUfkes <62268325+KevinUfkes@users.noreply.github.com>
2020-11-07 13:29:16 -0800

    Completed Adding User -> Set default Role to Client in user_role table

--- src/main/java/ca/gbc/comp3095/assignment2/repositories/RoleRepository.java
+++ src/main/java/ca/gbc/comp3095/assignment2/repositories/RoleRepository.java
@@ -2,7 +2,6 @@ package ca.gbc.comp3095.assignment2.repositories;
 
 import ca.gbc.comp3095.assignment2.domain.Role;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.Repository;
 
 public interface RoleRepository extends CrudRepository<Role, Long> {
 
--- src/main/java/ca/gbc/comp3095/assignment2/repositories/UserRepository.java
+++ src/main/java/ca/gbc/comp3095/assignment2/repositories/UserRepository.java
@@ -2,7 +2,6 @@ package ca.gbc.comp3095.assignment2.repositories;
 
 import ca.gbc.comp3095.assignment2.domain.User;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.Repository;
 
 public interface UserRepository extends CrudRepository<User, Long> {
 }
