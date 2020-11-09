0ba3a1175a0190c760e0f2704af8a2d656e68517
KevinUfkes <62268325+KevinUfkes@users.noreply.github.com>
2020-11-08 22:01:40 -0800

    removed email validation from username in User class

--- src/main/java/ca/gbc/comp3095/assignment2/domain/User.java
+++ src/main/java/ca/gbc/comp3095/assignment2/domain/User.java
@@ -44,9 +44,6 @@ public class User {
     @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$",message = "Password must contain at least: 1 uppercase, 1 lowercase, 1 number, 1 special character and must be at least 8 characters long!")
     private String password;
 
-    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$",message = "Password must contain at least: 1 uppercase, 1 lowercase, 1 number, 1 special character and must be at least 8 characters long!")
-    private String confirmPassword;
-
     @ManyToMany(mappedBy = "users")
     private Set<Role> roles = new HashSet<>();
 
