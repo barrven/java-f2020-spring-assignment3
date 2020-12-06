3c0847a1581e4fc04e23e6cf0bf05d549b35f8e6
thiago <thiagohissa@gmail.com>
2020-12-06 12:55:50 -0500

    support page added

---
+++ assignment3-web/src/main/java/ca/gbc/comp3095/assignment3/controllers/SupportController.java
@@ -0,0 +1,21 @@
+/* ******************************************************************************************
+ Project: COMP3095 Channel5NewsTeam
+ * Assignment: Assignment # 3
+ * Author(s): Kevin Ufkes, Barrington Venables, Thiago Hissa
+ * Student Number: 101197364, 101189284, 101176085
+ * Date: Saturday December 5, 2020
+ * Description: Allows a client to send message to support.
+ ********************************************************************************************/
+
+package ca.gbc.comp3095.assignment3.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SupportController {
+    @RequestMapping("/support")
+    public String responseUnderCTabs(){
+        return "client/support";
+    }
+}
