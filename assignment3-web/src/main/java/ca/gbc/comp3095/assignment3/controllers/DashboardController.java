120474d1359f9ccc10437ef1f0e8b9382607f565
Barrington <barrven@users.noreply.github.com>
2020-12-06 23:34:04 -0500

    implemented client dashboard

--- assignment3-web/src/main/java/ca/gbc/comp3095/assignment3/controllers/DashboardController.java
+++ assignment3-web/src/main/java/ca/gbc/comp3095/assignment3/controllers/DashboardController.java
@@ -9,8 +9,11 @@
 
 package ca.gbc.comp3095.assignment3.controllers;
 
+import ca.gbc.comp3095.assignment3.domain.Address;
+import ca.gbc.comp3095.assignment3.domain.Role;
 import ca.gbc.comp3095.assignment3.domain.SupportMessage;
 import ca.gbc.comp3095.assignment3.domain.User;
+import ca.gbc.comp3095.assignment3.services.AddressService;
 import ca.gbc.comp3095.assignment3.services.RoleService;
 import ca.gbc.comp3095.assignment3.services.SupportMessageService;
 import ca.gbc.comp3095.assignment3.services.UserService;
@@ -19,6 +22,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Controller
@@ -28,19 +34,43 @@ public class DashboardController {
     private final UserService userService;
     private final RoleService roleService;
     private final SupportMessageService supportMessageService;
+    private final AddressService addressService;
 
     public DashboardController(
-        UserService userService,
-        RoleService roleService,
-        SupportMessageService supportMessageService
-    ) {
+            UserService userService,
+            RoleService roleService,
+            SupportMessageService supportMessageService,
+            AddressService addressService) {
         this.roleService = roleService;
         this.userService = userService;
         this.supportMessageService = supportMessageService;
+        this.addressService = addressService;
     }
 
     @RequestMapping("/client")
-    public String loadClientDashboard() {
+    public String loadClientDashboard(Model model) {
+        User user = userService.findByUsername("client@isp.net");
+        Set<SupportMessage> messages = supportMessageService.findAll();
+
+        List<Address> temp = new ArrayList<>(addressService.findAll());
+        Set<Address> addresses = new HashSet<>();
+        for (Address address : temp) {
+            User u = address.getUser();
+            if (u.equals(user)) {
+                addresses.add(address);
+            }
+        }
+
+        Address address1 = addresses.iterator().next();
+        //Address address2 = addresses.iterator().next();
+        Address address2 = address1;
+
+        model.addAttribute("user", user);
+        model.addAttribute("messages", messages);
+        model.addAttribute("address1", address1);
+        model.addAttribute("address2", address2);
+
+
         return "client/dashboard";
     }
 
